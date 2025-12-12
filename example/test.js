a6cac0e4ec8491f967d75a249ed8949d07f536ff
bing <spiralladder@fastmail.com>
2025-12-12 09:10:54 +0800

    use ES module instead

--- example/test.js
+++ example/test.js
@@ -1,3 +1,6 @@
+import { createRequire } from 'node:module';
+
+const require = createRequire(import.meta.url);
 const example = require('../zig-out/lib/example.node');
 
 console.log(example.add(1, 2));
