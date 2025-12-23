50d9f044146be719ff6308a62e981b60462a21b4
Cayman <caymannava@gmail.com>
2025-12-23 11:15:30 -0500

    chore: remove unused code

--- src/Object.zig
+++
@@ -1,8 +0,0 @@
-const c = @import("c.zig");
-const status = @import("status.zig");
-const NapiError = @import("status.zig").NapiError;
-
-env: c.napi_env,
-value: c.napi_value,
-
-const Object = @This();
