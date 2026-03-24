fff894d0e045e20cc54c574ae83a39f310574486
bing <spiralladder@fastmail.com>
2026-03-24 19:25:55 +0800

    fix: pass in `null` for `napi_ref` in `napi_wrap`
    We're mostly discarding the returned `Ref` when calling `Env.wrap`,
    and this reference ends up never being cleaned up, which might be the
    cause of native memory leak in our bls integration in upstream lodestar.
    
    See: https://github.com/chainsafe/lodestar/pull/8900

--- src/Env.zig
+++ src/Env.zig
@@ -744,7 +744,6 @@ pub fn wrap(
     comptime finalize_cb: ?FinalizeCallback(Data),
     finalize_hint: ?*anyopaque,
 ) NapiError!Ref {
-    var ref_: c.napi_ref = undefined;
     try status.check(
         c.napi_wrap(
             self.env,
@@ -752,12 +751,12 @@ pub fn wrap(
             native_object,
             if (finalize_cb) |f| wrapFinalizeCallback(Data, f) else null,
             finalize_hint,
-            &ref_,
+            null,
         ),
     );
     return Ref{
         .env = self.env,
-        .ref_ = ref_,
+        .ref_ = null,
     };
 }
 
